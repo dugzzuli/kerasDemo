877b9b0654ba48d265d44a3921dcbacaa0da68ed
dugking <bingwei2642@qq.com>
2017-03-05 17:47:06 +0800

    Initial commit

---
+++ 01demo练习/loacmodel.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  2 12:59:25 2017
+
+@author: dugking
+"""
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.models import model_from_json
+import numpy
+
+from keras.utils import np_utils
+seed = 7
+numpy.random.seed(seed)
+# load data
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+# flatten 28*28 images to a 784 vector for each image
+num_pixels = X_train.shape[1] * X_train.shape[2]
+X_train = X_train.reshape(X_train.shape[0], num_pixels).astype('float32')
+X_test = X_test.reshape(X_test.shape[0], num_pixels).astype('float32')
+# normalize inputs from 0-255 to 0-1
+X_train = X_train / 255
+X_test = X_test / 255
+# one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+# load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+
+# evaluate loaded model on test data
+loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+dd=loaded_model.predict(X_test)
+print(X_test.shape)
---
+++ 01demo练习/multynu.py
@@ -0,0 +1,20 @@
+from  keras.models import Sequential
+from keras.layers import Dense
+import numpy as np
+seed=7
+np.random.seed(seed)
+dataset=np.loadtxt("pima-indians-diabetes.csv",delimiter=",")
+X=dataset[:,0:8]
+Y=dataset[:,8]
+model = Sequential()
+model.add(Dense(12, input_dim=8, init='uniform', activation='relu'))
+model.add(Dense(8, init='uniform', activation='relu'))
+model.add(Dense(1, init='uniform', activation='sigmoid'))
+# Compile model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# Fit the model
+model.fit(X, Y, nb_epoch=150, batch_size=10)
+a=range()
+# evaluate the model
+scores = model.evaluate(X, Y)
+print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
